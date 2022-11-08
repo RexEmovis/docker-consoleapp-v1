d9bd004249ce85e22fd6e2f7c04e11b425b0a4b2
RexEmovis <rex.ye@emovis.us>
2022-11-08 11:53:56 -0500

    initial comit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using App.WorkerService;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
